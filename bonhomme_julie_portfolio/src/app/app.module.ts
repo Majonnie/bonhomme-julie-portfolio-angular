1d1857fa81975c103456a840012d82e424a6b6de
Majonnie <bonhommejulie2003@gmail.com>
2022-04-10 23:45:35 +0200

    Début et avancement info + début responsive

--- bonhomme_julie_portfolio/src/app/app.module.ts
+++ bonhomme_julie_portfolio/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IdComponent } from './id/id.component';
 import { ProjectListComponent } from './project-list/project-list.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IdComponent,
-    ProjectListComponent
+    ProjectListComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
--- bonhomme_julie_portfolio/src/app/projects.ts
+++ bonhomme_julie_portfolio/src/app/projects.ts
@@ -2,6 +2,7 @@ export interface Project {
   id: number;
   name: string;
   tech: Array<string>;
+  date: string;
   description: string;
   image: string;
 }
@@ -11,6 +12,7 @@ export const projects = [
     id: 1,
     name: 'Projet 1',
     tech: ["C", " C", " C", " C"],
+    date: "Novembre 2022",
     description: 'DESC PROJET 1',
     image: "../assets/projets/red.png",
   },
@@ -18,6 +20,7 @@ export const projects = [
     id: 2,
     name: 'Projet 2',
     tech: ["Js", " Js", " Js", " Js"],
+    date: "Novembre 2022",
     description: null,
     image: null,
   },
@@ -25,6 +28,7 @@ export const projects = [
     id: 3,
     name: 'Projet 3',
     tech: ["C++", " C++", " C++", " C++"],
+    date: "Novembre 2022",
     description: "Description du projet, wow que de détails et de professionnalisme",
     image: null,
   },
