cba5ba48fcfe42b2fe11b0529d1a2f0eb5903c76
Majonnie <bonhommejulie2003@gmail.com>
2022-04-22 12:18:30 +0200

    Ajout images fiches projets

--- bonhomme_julie_portfolio/src/app/projects.ts
+++ bonhomme_julie_portfolio/src/app/projects.ts
@@ -18,7 +18,7 @@ export const projects = [
     date: "Septembre 2022",
     description: "Jeu en lignes de commandes de combats en tour par tour basé sur l'univers de Zelda." +
       " Le joueur doit affronter des monstres puis un boss et peut choisir d’attaquer ou de se soigner à chaque tour.",
-    image: "../assets/projets/red.png",
+    image: "../assets/projets/hyrule_castle.png",
     lien_VM: null,
     lien_GH: "https://github.com/Majonnie/hyrule-castle",
   },
@@ -29,9 +29,9 @@ export const projects = [
     date: "Novembre 2021",
     description: "Jeu consistant à obtenir un maximum de ressources en cliquant" +
       " répétitivement sur un bouton et en achetant des améliorations.",
-    image: "../assets/projets/red.png",
+    image: "../assets/projets/clicker_game.png",
     lien_VM: "TRUC",
-    lien_GH: null,
+    lien_GH: "https://github.com/Majonnie/MyClickerGame",
   },
   {
     id: 3,
@@ -39,10 +39,10 @@ export const projects = [
     tech: ["HTML", " CSS", " PHP (Laravel)"],
     date: "Novembre 2021",
     description: "Création d'un site d'e-commerce à l'aide du framework Laravel : catalogue trié par pays," +
-      "gestion des produits pour les administrateurs, panier, profil de l'utilisateur avec historique des commandes...",
-    image: "../assets/projets/red.png",
+      " gestion des produits pour les administrateurs, panier, profil de l'utilisateur avec historique des commandes...",
+    image: "../assets/projets/liberty_commerce.png",
     lien_VM: "TRUC",
-    lien_GH: null,
+    lien_GH: "https://github.com/Majonnie/LibertyCommerce",
   },
   {
     id: 4,
@@ -50,7 +50,7 @@ export const projects = [
     tech: ["HTML", " CSS", " PHP (Symfony)"],
     date: "Décembre 2021",
     description: "Création d'un outil de génération de factures afin de répondre à un besoin d'organisation de l'entreprise.",
-    image: "../assets/projets/red.png",
+    image: "../assets/projets/site_J2B.png",
     lien_VM: "TRUC",
     lien_GH: "https://github.com/garoNits/J2BCamp",
   },
@@ -60,9 +60,9 @@ export const projects = [
     tech: ["C"],
     date: "Janvier 2022",
     description: "Réimplémentation de la commande ls en C.",
-    image: "../assets/projets/red.png",
+    image: "../assets/projets/my_ls.png",
     lien_VM: null,
-    lien_GH: null,
+    lien_GH: "https://github.com/Majonnie/my_ls",
   },
   {
     id: 6,
@@ -70,8 +70,8 @@ export const projects = [
     tech: ["Godot", " GDScript"],
     date: "Mars 2022",
     description: "Création d'un jeu-vidéo rogue-like (en travaillant principalement sur la construction de maps avec les tilemaps," +
-      "le design et les animations du personnage et des ennemis, la caméra et les mouvements).",
-    image: "../assets/projets/red.png",
+      " le design et les animations du personnage et des ennemis, la caméra et les mouvements).",
+    image: "../assets/projets/dream_travel.png",
     lien_VM: null,
     lien_GH: "https://github.com/Majonnie/CodeCampsGraven/tree/fusion-back",
   },
